bc5d169800ae1bb650794bc621474648bc133b7f
Praneeth <badripraneeth785@gmail.com>
2026-07-30 19:04:08 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n= s.size(),m = t.size();
+        if(n!=m){
+            return false;
+        }
+        std::map<char,int> x;
+        std::map<char,int> y;
+        for(int i=0;i<n;i++){
+            x[s[i]]+=1;
+            y[t[i]]+=1;
+        }
+        return x==y;
+        
+        
+    }
+};
